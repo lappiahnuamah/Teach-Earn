2693963fd3e70cac832901f6acbb407da9bd04b4
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2021-08-16 00:40:10 +0100

    Adding some few template files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignUpView, home
+from .views import register, index, about, login
 
 urlpatterns = [
-  path('register/', SignUpView.as_view(), name='signup'),
-  path('', home, name='home'),
+  # path('register/', SignUpView.as_view(), name='signup'),
+  path('register/', register, name='register'),
+  path('about/', about, name='about'),
+  path('login/', login, name='login'),
+  path('', index, name='index'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,11 +4,27 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+# class SignUpView(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'registration/signup.html'
 
 
-def home(request):
-    return render(request, 'home.html', {})
+def index(request):
+    return render(request, 'registration/index.html', {})
+
+    
+def about(request):
+    return render(request, 'registration/about.html', {})
+
+
+def login(request):
+    return render(request, 'registration/login.html', {})
+
+
+def register(request):
+    return render(request, 'registration/register.html', {})
+
+
+def forgotten_password(request):
+    return render(request, 'registration/forgotten_password.html', {})
